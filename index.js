a2a22f1c5f6974e8e7a6f8e71a473147c589cdd6
James <gouser101@gmail.com>
2021-09-23 01:09:23 +0100

    fixed deployment bugs

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const PORT = process.env.PORT || 3001;
+require('dotenv').config();
 
 app.use(express.json());
 app.use(cors());
@@ -21,7 +21,7 @@ app.use('/favourite', favouritesRouter);
 // listen to port and check models for tables
 
 db.sequelize.sync().then(() => {
-  app.listen(PORT, () => {
+  app.listen(process.env.PORT || 3001, () => {
     console.log(`Listening on Port: ${PORT}`);
   });
 });
