2e7d6c901a6925286c3d2bfecf6d75d07816de7b
James <gouser101@gmail.com>
2023-05-11 19:29:42 +0100

    Updated index.js to include cors access

--- index.js
+++ index.js
@@ -6,6 +6,15 @@ require('dotenv').config();
 app.use(express.json());
 app.use(cors());
 
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
+
 const db = require('./models');
 
 // Routers
